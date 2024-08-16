9f71b243c8581c54c25d7770698fc5315d14f67f
Sergoman4 <28062002sergey@gmail.com>
2024-08-17 01:19:29 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,7 +3,13 @@ class Animal:
         self.alive = True
         self.fed = False
         self.name = name
-
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Plant:
     def __init__(self, name):
@@ -15,26 +21,12 @@ class Mammal(Animal):
     def __init__(self, name):
         super().__init__(name)
 
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-
+ 
 
 class Predator(Animal):
     def __init__(self, name):
         super().__init__(name)
 
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
 
 
 class Flower(Plant):
